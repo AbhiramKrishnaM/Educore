ac732bee46a00650dfda8e407be820c5fec50cf2
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-08-13 13:47:47 +0530

    feat: added illustration

--- components/Home/Header.vue
+++ components/Home/Header.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="wrapper pa-8 rounded-xl d-flex align-center white--text">
+  <div
+    class="wrapper pa-8 rounded-xl d-flex justify-space-between align-center white--text"
+  >
+    <div class="background-polygon"></div>
     <h1>
       Your students are doing great <span>60%</span> students has<br />
       completed the test.
     </h1>
-
-    <div class="background-polygon"></div>
+    <div class="illustration-wrapper mr-6">
+      <v-img
+        height="100%"
+        width="100%"
+        :src="require('@/assets/images/Kid.svg')"
+        alt="Kid learning "
+        class="image"
+      ></v-img>
+    </div>
   </div>
 </template>
 <script>
@@ -15,18 +25,15 @@ export default {
 </script>
 <style scoped>
 .wrapper {
-  z-index: 10;
   width: 100%;
   height: 20vh;
-  overflow: hidden;
   background-color: #4540e1;
   position: relative;
 }
 
 h1 {
-  z-index: 12;
   font-weight: 300;
-  font-size: 30px;
+  font-size: 27px;
   line-height: 1.4em;
 }
 
@@ -36,7 +43,6 @@ span {
 }
 
 .background-polygon {
-  z-index: 11;
   height: 400px;
   width: 200px;
   position: absolute;
@@ -44,5 +50,16 @@ span {
   background: linear-gradient(117.45deg, #ffffff36 5.89%, #fff0 75.99%);
   clip-path: polygon(0 0, 52% 0, 100% 100%, 75% 100%);
   transform: rotate(-40deg);
+}
+
+.illustration-wrapper {
+  position: relative;
+  height: 210px;
+  width: 210px;
+}
+
+.image {
+  position: absolute;
+  bottom: 10%;
 }
 </style>
