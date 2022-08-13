1db0a10c0a22ab930ce37597ab37157d7252c843
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-08-13 15:15:37 +0530

    feat: added drop down list to the test submission section

--- components/Home/TestSubmission.vue
+++ components/Home/TestSubmission.vue
@@ -4,16 +4,46 @@
       <h1>Submitted Tests</h1>
       <span
         >Class 6th
-        <v-btn icon
-          ><v-icon color="#DBDBDB">mdi-chevron-down</v-icon></v-btn
-        ></span
-      >
+
+        <v-menu
+          transition="slide-x-transition"
+          :close-on-click="false"
+          nudge-left="60"
+          nudge-bottom="10"
+          bottom
+          offset-y
+        >
+          <template #activator="{ on, attrs }">
+            <v-btn icon
+              ><v-icon color="#DBDBDB" v-bind="attrs" v-on="on"
+                >mdi-chevron-down</v-icon
+              ></v-btn
+            >
+          </template>
+
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index">
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </span>
     </div>
   </div>
 </template>
 <script>
 export default {
   name: 'HomeTestSubmission',
+  data() {
+    return {
+      items: [
+        { title: 'Class 7th' },
+        { title: 'Class 8th' },
+        { title: 'Class 9th' },
+        { title: 'Class 10th' },
+      ],
+    }
+  },
 }
 </script>
 <style scoped>
