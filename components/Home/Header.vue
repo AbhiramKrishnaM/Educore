efd9ff18c48c291768fab75c10a7d009246ea32d
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-08-13 14:13:57 +0530

    feat: adeed illustration. animation not added, media queries not added

--- components/Home/Header.vue
+++ components/Home/Header.vue
@@ -3,11 +3,16 @@
     class="wrapper pa-8 rounded-xl d-flex justify-space-between align-center white--text"
   >
     <div class="background-polygon"></div>
+
     <h1>
       Your students are doing great <span>60%</span> students has<br />
       completed the test.
     </h1>
     <div class="illustration-wrapper mr-6">
+      <div class="halo first"></div>
+      <div class="halo second"></div>
+      <div class="halo third"></div>
+
       <v-img
         height="100%"
         width="100%"
@@ -61,5 +66,31 @@ span {
 .image {
   position: absolute;
   bottom: 10%;
+}
+
+.halo {
+  position: absolute;
+  border-radius: 100%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.first {
+  width: 230px;
+  height: 230px;
+  background: linear-gradient(180deg, #ffffff3d 0%, #fff0 100%);
+}
+
+.second {
+  width: 200px;
+  height: 200px;
+  background: linear-gradient(180deg, #ffffff1a 0%, #fff0 100%);
+}
+
+.third {
+  width: 170px;
+  height: 170px;
+  background: linear-gradient(180deg, #ffffff1f 46.95%, #fff0 100%);
 }
 </style>
