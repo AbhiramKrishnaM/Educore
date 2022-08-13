56f4fcb3c6b623ce495d2f6f1f163a675f88d589
Abhiram krishna <66724040+AbhiramKrishnaM@users.noreply.github.com>
2022-08-13 13:18:25 +0530

    feat: added header detail, added clip polygon

--- components/Home/Header.vue
+++ components/Home/Header.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="wrapper rounded-xl"></div>
+  <div class="wrapper pa-8 rounded-xl d-flex align-center white--text">
+    <h1>
+      Your students are doing great <span>60%</span> students has<br />
+      completed the test.
+    </h1>
+
+    <div class="background-polygon"></div>
+  </div>
 </template>
 <script>
 export default {
@@ -8,8 +15,34 @@ export default {
 </script>
 <style scoped>
 .wrapper {
+  z-index: 10;
   width: 100%;
-  background-color: orange;
   height: 20vh;
+  overflow: hidden;
+  background-color: #4540e1;
+  position: relative;
+}
+
+h1 {
+  z-index: 12;
+  font-weight: 300;
+  font-size: 30px;
+  line-height: 1.4em;
+}
+
+span {
+  font-weight: bold;
+  font-size: 40px;
+}
+
+.background-polygon {
+  z-index: 11;
+  height: 400px;
+  width: 200px;
+  position: absolute;
+  left: 3%;
+  background: linear-gradient(117.45deg, #ffffff36 5.89%, #fff0 75.99%);
+  clip-path: polygon(0 0, 52% 0, 100% 100%, 75% 100%);
+  transform: rotate(-40deg);
 }
 </style>
